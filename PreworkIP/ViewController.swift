fae917c7103632ce90804946511426cfe8cd2db8
ishap04 <110761965+ishap04@users.noreply.github.com>
2022-08-11 23:17:51 -0400

    Added Completed Prework Files to Repository

---
+++ PreworkIP/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PreworkIP
+//
+//  Created by Isha Patel on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("Hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
